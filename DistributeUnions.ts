58aa59454f4ad84a6fe9a07b86dc16ec74bc0efe
Andrea Simone Costa <jfet97@users.noreply.github.com>
2023-12-24 11:13:31 +0100

    Update DistributeUnions.ts

--- DistributeUnions.ts
+++ DistributeUnions.ts
@@ -18,29 +18,39 @@ type T3 = DistributeUnions<[{ value: 'a' | 'b' }, { x: { y: 2 | 3 } }] | 17>
 type _DistributeUnions<T> = {
   [K in keyof T]: {
     PROP: T[K] extends infer U //  U = T[K]
-    ? U extends any
-    ? { [KK in K]: U }
-    : never
+      ? U extends any
+        ? { [KK in K]: U }
+        : never
     : never;
   }
 }
 
-export type DistributeUnions<
+type test = UnionToIntersection<_DistributeUnions<[1 | 2, 'a' | 'b']>[number]>
+
+type DistributeUnions<
   T,
   DT = { [K in keyof T]: DistributeUnions<T[K]> }
   > = DT extends object
-  ? MergeInsertions<
-    LooseLookup<UnionToIntersection<_DistributeUnions<DT>[keyof DT]>, "PROP">
-  > extends infer RES
-  ? RES extends any
-  ? DT extends readonly any[]
-  ? { [K in keyof DT]: LooseLookup<RES, K> /* RES[K] */ }
-  : RES
-  : never
-  : never
-  : DT;
+    ? MergeInsertions<
+        LooseLookup<
+          UnionToIntersection<
+            LooseLookup<
+              _DistributeUnions<DT>,
+              DT extends readonly any[] ? number : keyof DT
+            >
+          >,
+          "PROP"
+        >
+      > extends infer RES
+        ? RES extends any
+          ? DT extends readonly any[]
+            ? { [K in keyof DT]: LooseLookup<RES, K> /* RES[K] */ }
+            : RES
+          : never
+        : never
+    : DT;
 
-// Proprietà fondamentale: (A | B) & C = (A & C) | (B & C)
+// Proprietà fondamentale 1: (A | B) & C = (A & C) | (B & C)
 // Proprietà fondamentale 2: { prop: T } & { prop: U } = { prop: T & U }
 // Proprietà fondamentale 3: ({ a: T, b: U })["a" | "b"] = T | U
 
